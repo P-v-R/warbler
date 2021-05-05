436bbcfec402db6c84eed74f991e4aa9078f6947
Kiru <kirushanth.ravindran@gmail.com>
2021-05-05 12:04:21 -0700

    finished Step 5

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -213,7 +213,35 @@ def profile():
     """Update profile for current user."""
 
     # IMPLEMENT THIS
-    # /users/edit.html
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    form = UserEditForm(obj=g.user)
+
+    if form.validate_on_submit():
+        user = User.authenticate(g.user.username,
+                                 form.password.data)
+
+        if user:
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data
+            user.header_image_url = form.header_image_url.data
+            user.bio = form.bio.data
+
+            db.session.commit()
+
+            flash("Successfully editted profile!", "success")
+            return redirect(f'/users/{user.id}')
+
+        else:
+            flash("Incorrect Password!", "danger")
+            return render_template('/users/edit.html', form=form)
+
+    else:
+        return render_template('/users/edit.html', form=form) 
+
 
 
 @app.route('/users/delete', methods=["POST"])
